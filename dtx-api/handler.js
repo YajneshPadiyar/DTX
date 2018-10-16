0c2250e59691a2d737283c4ed0a4d08fd691b0cd
Nimish Bidwai <NIMISHB@amdocs.com>
2018-10-15 18:07:40 -0700

    Added new API's for Department Schema

--- dtx-api/handler.js
+++ dtx-api/handler.js
@@ -1,5 +1,6 @@
 const connectToDatabase = require('./db');
 const Note = require('./models/Note');
+const Department = require('./models/Department');
 require('dotenv').config({ path: './variables.env' });
 
 module.exports.create = (event, context, callback) => {
@@ -89,5 +90,77 @@ module.exports.delete = (event, context, callback) => {
           headers: { 'Content-Type': 'text/plain' },
           body: 'Could not fetch the notes.'
         }));
+    });
+};
+
+module.exports.createDepartment = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Department.create(JSON.parse(event.body))
+        .then(department => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(department)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not create the Department.' + err
+        }));
+    });
+};
+
+module.exports.getDepartmentByName = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Department.find({'information.name':event.pathParameters.name})
+        .then(department => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(department)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the department.'
+        }));
+    });
+};
+
+module.exports.getDepartmentById = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Department.findById(event.pathParameters.id)
+        .then(department => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(department)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the department.'
+        }));
+    });
+};
+
+module.exports.getAllDepartments = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      Department.find()
+        .then(departments => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(departments)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the departments.'
+        }))
     });
 };
